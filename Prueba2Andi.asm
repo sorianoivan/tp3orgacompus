d63b71a1db015d087885bea546e109cd937be4f7
MarcosRolando <marcosrolando.mr@gmail.com>
2020-08-06 20:08:07 -0300

    .

--- Prueba2Andi.asm
+++ Prueba2Andi.asm
@@ -1,6 +1,6 @@
 #Prueba 2 Andi
 
 li $t1, -3
-andi $t2, $t1, -3 #Carga -3 en t2
-li $t3, 0xFFFF
-andi $t1, $t2, 
+andi $t2, $t1, -3 #Queda -3 en t2
+li $t3, 0x8000 #Queda en -2^15 t3
+andi $t1, $t3, 0xFFFF #Queda en -2^15 t1
